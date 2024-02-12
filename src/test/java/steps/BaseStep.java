56dd27721d1474feb39a2ab4cec6206ee6de3a52
maratbikkulov <marat09022003@gmail.com>
2024-02-12 11:17:51 +0100

    Удалил лишние файлы

--- src/test/java/steps/BaseStep.java
+++
@@ -1,9 +0,0 @@
-package steps;
-
-import java.util.Random;
-
-public abstract class BaseStep {
-    public String getRandomProjectId() {
-        return String.valueOf(new Random().nextInt(1000000000));
-    }
-}
