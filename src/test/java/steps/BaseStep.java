295c1ec7e3c9e909f2c4e3d3fc2c3900634439a3
maratbikkulov <marat09022003@gmail.com>
2024-02-16 20:48:17 +0100

    Fixed problem with notification

--- src/test/java/steps/BaseStep.java
+++ src/test/java/steps/BaseStep.java
@@ -1,5 +1,22 @@
 package steps;
 
+import org.openqa.selenium.interactions.Actions;
+
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+
 public abstract class BaseStep {
 
+    private static final String CONTAINER_IFRAME_XPATH = "//*[@id='intercom-container']//iframe";
+
+    private final String CLOSE_NOTIFICATION_XPATH = "//*[contains(text(), 'Clear')]";
+
+    public void closeNotificationsIfExist() {
+        if ($x(CONTAINER_IFRAME_XPATH).isDisplayed()) {
+            getWebDriver().switchTo().frame($x(CONTAINER_IFRAME_XPATH));
+            new Actions(getWebDriver()).moveToElement($x(CLOSE_NOTIFICATION_XPATH)).build().perform();
+            $x(CLOSE_NOTIFICATION_XPATH).click();
+            getWebDriver().switchTo().defaultContent();
+        }
+    }
 }
